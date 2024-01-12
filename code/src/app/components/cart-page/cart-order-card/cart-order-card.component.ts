ebd74e7803cebdb908f39401e06e84090d78d00b
jekasrs <jekasjeny1012@gmail.com>
2024-01-12 09:45:52 +0100

    cart page and cart's components: user-service realised

--- code/src/app/components/cart-page/cart-order-card/cart-order-card.component.ts
+++ code/src/app/components/cart-page/cart-order-card/cart-order-card.component.ts
@@ -3,6 +3,7 @@ import {IOrder} from "../../../models/order";
 import {IItemQuantity} from "../../../models/item_quantity";
 import {items} from "../../../data/items";
 import {Router} from "@angular/router";
+import {UserService} from "../../../services/user.service";
 
 @Component({
   selector: 'app-cart-order-card',
@@ -10,28 +11,33 @@ import {Router} from "@angular/router";
   styleUrls: ['./cart-order-card.component.css']
 })
 export class CartOrderCardComponent {
-  title: 'cart-order-card'
-  @Input() order: IOrder
-  data: IItemQuantity[]
-  price: number = 0
-  checkboxChecked: boolean = true
-  hasRecipeItems: boolean = false
+  title: 'cart-order-card';
+  @Input() order: IOrder;
+  data: IItemQuantity[];
+  price: number = 0;
+  checkboxChecked: boolean = true;
+  hasRecipeItems: boolean = false;
+  private userId: number = -1;
 
-  constructor(private router: Router) {
+  constructor(private userService: UserService, private router: Router) {
     window.addEventListener('load', () => {
       this.hasRecipeOnlyItems();
     })
   }
 
+  ngOnInit() {
+    this.userService.currentUserId.subscribe((userId) => (this.userId = userId));
+  }
+
   calculatePrice() {
     this.data = this.order.items;
     this.price = 0;
-    for (let i = 0; i < this.data.length; i++)
-    {
-      this.price += items[this.data[i].itemId].cost * this.data[i].itemQuantity
+    for (let i = 0; i < this.data.length; i++) {
+      this.price += items[this.data[i].itemId].cost * this.data[i].itemQuantity;
     }
     return this.price.toString();
   }
+
   createOrder() {
     if (this.checkboxChecked) {
       this.router.navigate(['/order-page']);
@@ -39,8 +45,7 @@ export class CartOrderCardComponent {
         id: '', date: '', address: '', deliverDate: '',
         price: this.price, orderNumber: '', items: this.order.items
       }
-    }
-    else return null;
+    } else return null;
   }
 
   hasRecipeOnlyItems() {
@@ -49,32 +54,35 @@ export class CartOrderCardComponent {
         this.hasRecipeItems = true;
         this.checkboxChecked = false;
         document.querySelectorAll(".card-order")
-          .forEach( orderCard=> {
+          .forEach(orderCard => {
             orderCard.querySelectorAll(".button-color")
               .forEach(button => {
                 button.classList.add('inactive')
-            })})
-        return }})
+              })
+          })
+        return
+      }
+    })
   }
 
   changeOrderButtonColor() {
     this.checkboxChecked = !this.checkboxChecked;
     if (this.checkboxChecked) {
       document.querySelectorAll(".card-order")
-        .forEach( orderCard=> {
+        .forEach(orderCard => {
           orderCard.querySelectorAll(".button-color")
             .forEach(button => {
               button.classList.remove('inactive')
             })
         })
-    }
-    else {
+    } else {
       document.querySelectorAll(".card-order")
-        .forEach( orderCard=> {
+        .forEach(orderCard => {
           orderCard.querySelectorAll(".button-color")
             .forEach(button => {
               button.classList.add('inactive')
-            })})
+            })
+        })
     }
   }
 }
--- code/src/app/components/cart-page/cart.component.ts
+++ code/src/app/components/cart-page/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { IOrder } from "../../models/order";
 import { Router } from '@angular/router';
 import {orders} from "../../data/orders";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-cart',
@@ -9,16 +10,22 @@ import {orders} from "../../data/orders";
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-  @Input() order: IOrder = orders[0]
-  isEmpty: boolean
+  @Input() order: IOrder = orders[0];
+  isEmpty: boolean;
 
-  constructor(private router: Router) {
+  private userId: number = -1;
+
+  constructor(private userService: UserService, private router: Router) {
     window.addEventListener("load", () => {
       this.isEmpty = this.checkCart() })
-    }
+  }
 
-  checkCart()
-  {
+  ngOnInit(){
+    this.userService.currentUserId.subscribe((userId) => (this.userId = userId));
+    console.log(this.userId);
+  }
+
+  checkCart() {
     return this.order.items.length == 0;
   }
 
