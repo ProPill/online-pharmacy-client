d44e754da3aa174c90408b44762e51cc1c0580ea
maria.ivanova <maria.ivanova@vk.team>
2024-01-14 16:33:56 +0300

    added backend service into account page

--- code/src/app/components/cart-page/cart-item-card/cart-item-card.component.ts
+++ code/src/app/components/cart-page/cart-item-card/cart-item-card.component.ts
@@ -4,6 +4,7 @@ import {IItemQuantity} from "../../../models/item_quantity";
 import {items} from "../../../data/items";
 import {UserService} from "../../../services/user.service";
 import {Router} from "@angular/router";
+import {BackendService} from "../../../services/backend.service";
 
 @Component({
   selector: 'app-cart-item-card',
@@ -20,8 +21,10 @@ export class CartItemCardComponent {
 
   private userId: number = -1;
 
-  constructor(private userService: UserService, private router: Router) {
+  constructor(private backendService: BackendService, private userService: UserService, private router: Router) {
     this.userService.currentUserId.subscribe((userId) => (this.userId = userId));
+    // this.item = this.backendService.getItemData(this.item.id); ждет своего часа
+    this.quantity = this.itemQuantuty.itemQuantity;
   }
 
   getTitle() {
