9d3f26aceca370c1125d4b86b4280edc1134b5c1
maria.ivanova <maria.ivanova@vk.team>
2024-01-14 16:33:44 +0300

    added backend service into cart

--- code/src/app/components/cart-page/cart.component.ts
+++ code/src/app/components/cart-page/cart.component.ts
@@ -3,6 +3,7 @@ import { IOrder } from "../../models/order";
 import { Router } from '@angular/router';
 import {orders} from "../../data/orders";
 import {UserService} from "../../services/user.service";
+import {BackendService} from "../../services/backend.service";
 
 @Component({
   selector: 'app-cart',
@@ -10,14 +11,15 @@ import {UserService} from "../../services/user.service";
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-  @Input() order: IOrder = orders[0];
+  @Input() order: IOrder;
   isEmpty: boolean;
 
   private userId: number = -1;
 
-  constructor(private userService: UserService, private router: Router) {
+  constructor(private backendService: BackendService, private userService: UserService, private router: Router) {
     this.userService.currentUserId.subscribe((userId) => (this.userId = userId));
     window.addEventListener("load", () => {
+      this.order = this.backendService.getCartPageData(this.userId)
       this.isEmpty = this.checkCart() })
   }
 
