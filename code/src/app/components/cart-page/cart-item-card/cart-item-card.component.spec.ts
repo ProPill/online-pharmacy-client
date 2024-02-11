0048855bf660e70f318d8b6448b66e3e092327e9
maria.ivanova <maria.ivanova@vk.team>
2024-02-12 00:44:01 +0300

    cart item card tests

--- code/src/app/components/cart-page/cart-item-card/cart-item-card.component.spec.ts
+++ code/src/app/components/cart-page/cart-item-card/cart-item-card.component.spec.ts
@@ -3,19 +3,57 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CartItemCardComponent } from './cart-item-card.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {BackendService} from "../../../services/backend.service";
+import {UserService} from "../../../services/user.service";
+import {items} from "../../../data/items";
+import {IItemQuantity} from "../../../models/item_quantity";
+import {IItem} from "../../../models/item";
+import {of} from "rxjs";
 
 describe('CartItemCardComponent', () => {
   let component: CartItemCardComponent;
   let fixture: ComponentFixture<CartItemCardComponent>;
 
+// мокируем сервисы
+  const userServiceMock = {
+    currentUserId: of(-1),
+  };
+
+  const backendServiceMock = {
+    addToCartItem: jasmine.createSpy('addToCartItem'),
+    deleteItemFromOrder: jasmine.createSpy('deleteItemFromOrder'),
+    getCartPageData: jasmine.createSpy('getCartPageData'),
+  };
+
+  const itemQuantityMock: IItemQuantity = {
+    itemId: items[0].id,
+    itemQuantity: 2,
+    hasRecipe: items[0].recipeOnly
+  };
+
+  const itemMock: IItem = {
+    id: 0,
+    title: 'Аквалор софт duo 150 мл душ/струя',
+    manufacturer: 'Швеция',
+    recipeOnly: true,
+    special: false,
+    cost: 850.00,
+    image: 'assets/pictures/order-success-img.png'
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [CartItemCardComponent],
-      imports: [ HttpClientTestingModule ],
-      providers: [ BackendService ]
+      providers: [
+        { provide: UserService, useValue: userServiceMock },
+        { provide: BackendService, useValue: backendServiceMock },
+      ],
     });
+
     fixture = TestBed.createComponent(CartItemCardComponent);
     component = fixture.componentInstance;
+    component.itemQuantity = itemQuantityMock;
+    component.item = itemMock;
     fixture.detectChanges();
   });
 
@@ -24,29 +62,54 @@ describe('CartItemCardComponent', () => {
   });
 
   it('should return title of the item', () => {
-    expect();
+    const title = component.getTitle();
+    expect(title).toEqual(itemMock.title);
   })
 
-  it('should increase quantity of the item', () => {
-    let before = component.quantity;
+  it('should increase quantity and call addToCartItem on increaseQuantity()', () => {
+    const initialQuantity = component.quantity;
     component.increaseQuantity();
-    expect(component.quantity - before).toBe(1);
-    expect(component.quantity).toBe(component.itemQuantity.itemQuantity)
-  })
+    const expectedQuantity = initialQuantity + 1;
+    expect(component.quantity).toEqual(expectedQuantity);
+    expect(backendServiceMock.addToCartItem)
+        .toHaveBeenCalledWith(-1, itemMock.id, 1);
+  });
 
-  it('should decrease quantity of the item', () => {
-    let before = component.quantity;
+  it('should decrease quantity, call addToCartItem, if quantity more than 1 on decreaseQuantity()', () => {
+    component.quantity = 2;
+    const initialQuantity = component.quantity;
     component.decreaseQuantity();
-    if (before == 1) {
-      expect(before).toBe(component.quantity)
+    const expectedQuantity = initialQuantity - 1;
+    expect(component.quantity).toEqual(expectedQuantity);
+    expect(backendServiceMock.addToCartItem)
+        .toHaveBeenCalledWith(-1, itemMock.id, -1);
+  });
+
+  it('should not decrease quantity and call addToCartItem, if quantity equals 1 on decreaseQuantity()', () => {
+    // try to decrease below 1
+    while (component.quantity > 1)
+    {
+      component.decreaseQuantity();
     }
-    else {
-      expect(before - component.quantity).toBe(1);
-    }
-    expect(component.quantity).toBe(component.itemQuantity.itemQuantity)
+    component.decreaseQuantity();
+    expect(component.quantity).toEqual(1);
+    expect(backendServiceMock.addToCartItem)
+        .toHaveBeenCalledWith(-1, itemMock.id, -1);
+  });
+
+  it('should calculate cost correctly on calculateCost()', () => {
+    component.quantity = itemQuantityMock.itemQuantity;
+    const expectedCost = itemMock.cost * component.quantity;
+    const calculatedCost = component.calculateCost();
+
+    expect(calculatedCost).toEqual(expectedCost);
+    expect(component.cost).toEqual(expectedCost);
   })
 
-  it('should calculate cost', () => {
+  it('should call deleteItemFromOrder and getCartPageData on deleteItem()', () => {
+    component.deleteItem();
 
-  })
+    expect(backendServiceMock.deleteItemFromOrder).toHaveBeenCalledWith(itemMock.id);
+    expect(backendServiceMock.getCartPageData).toHaveBeenCalledWith(-1);
+  });
 });
