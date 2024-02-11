2144716e7a6be64d83588bcd4ad05d1a079f90c1
maria.ivanova <maria.ivanova@vk.team>
2024-02-11 12:02:13 +0300

    written first tests for cart item

--- code/src/app/components/cart-page/cart-item-card/cart-item-card.component.spec.ts
+++ code/src/app/components/cart-page/cart-item-card/cart-item-card.component.spec.ts
@@ -18,4 +18,31 @@ describe('CartItemCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return title of the item', () => {
+    expect();
+  })
+
+  it('should increase quantity of the item', () => {
+    let before = component.quantity;
+    component.increaseQuantity();
+    expect(component.quantity - before).toBe(1);
+    expect(component.quantity).toBe(component.itemQuantity.itemQuantity)
+  })
+
+  it('should decrease quantity of the item', () => {
+    let before = component.quantity;
+    component.decreaseQuantity();
+    if (before == 1) {
+      expect(before).toBe(component.quantity)
+    }
+    else {
+      expect(before - component.quantity).toBe(1);
+    }
+    expect(component.quantity).toBe(component.itemQuantity.itemQuantity)
+  })
+
+  it('should calculate cost', () => {
+
+  })
 });
